6a63af95f28526c9ab7bda346bb71981d8d353ba
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2019-01-15 18:28:44 +0100

    npm and git auth stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+
+git config credential.helper '!f() { sleep 1; echo "username=${GIT_USER}\npassword=${GIT_PASSWORD}"; }; f'
+
 npm update && node index.js
