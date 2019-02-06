701255b4479fcfea343ca694c27fd8766dcbab62
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2019-02-06 16:48:55 +0100

    hopefully NOW git works

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# set git info from env vars
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
+
+# set npm token from env var
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
+# update dependencies (we want the latest authcode) and run.
 npm update && node index.js
